9cb74c728c1f87380175eeeada7bcd3852d0ce12
Muzammil_Hussain <muzammilchuhan.lb@gmail.com>
2022-06-11 19:05:19 +0500

    test add

--- auctions/tests.py
+++ auctions/tests.py
@@ -2,8 +2,9 @@ import unittest
 from django.test import TestCase
 
 
-class Check(unittest.TestCase):
-    def check_test():
+class Check_test(unittest.TestCase):
+
+    def test_check(self):
         x = 12
         assert(x+x == 24)
 
